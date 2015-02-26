1b39a87ad021217ecc75a25a0fbf985ec06aee6f
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-02-25 23:15:00 -0500

    Moving 'POST /register' endpoint to 'POST /users' and  'POST /login' to 'POST /users/login'. Deleting redundant AuthController.

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -4,8 +4,53 @@
  * @description :: Server-side logic for managing users
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
+var jwt = require('jwt-simple');
+var secret = Secret['jwt-secret'];
 
 module.exports = {
-	
+	login: function(request, response, next) {
+		if (request.body) {
+			var expires = 60*60*24*7*1000 + Date.now();
+
+			User.findOne({username: request.body.username})
+				.then(function(user){
+					if (! user) {
+						response.end('User does not exist.');
+						return;
+					}
+
+					return user.authenticate(request.body.password)
+						.then(function(isMatch) {
+							if (! isMatch) {
+								response.end('Invalid password.');
+								return;
+							} 
+
+							// return jwt
+							return jwt.encode({
+								iss: user.id,
+								exp: expires,
+							}, secret);
+
+						});
+				})
+				.catch(function(error) {
+					response.status(500).json({
+						title: 'Failed to login.',
+						reason: error
+					})
+				})
+				.done(function(token) {
+					token && response.status(200).json({
+						token: token,
+						expires: expires
+					})
+				});
+		} 
+		else {
+			response.badRequest();
+			return;
+		}
+	}
 };
 
--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,9 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
-  }
+  },
+
+  'POST /users/login': 'AuthController.login',
 
   /***************************************************************************
   *                                                                          *
