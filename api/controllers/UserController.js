bbba1ab73a608e47e0e66118604ef4c82f9ee33d
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-02-28 17:23:31 -0500

    Switching to using Sails custom responses instead of response.{json, end, ...}.

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -9,22 +9,23 @@ var secret = Secret['jwt-secret'];
 
 module.exports = {
 	login: function(request, response, next) {
+
 		if (request.body && request.body.user) {
 			var expires = 60*60*24*7*1000 + Date.now();
 
 			User.findOne({username: request.body.user.username})
 				.then(function(user){
 					if (! user) {
-						response.status(401).end('User does not exist.');
+						response.forbidden({ reason: 'User does not exist.'});
 						return;
 					}
 
 					return user.authenticate(request.body.user.password)
 						.then(function(isMatch) {
 							if (! isMatch) {
-								response.status(401).end('Invalid password.');
+								response.forbidden({ reason: 'Invalid password.'});
 								return;
-							} 
+							}
 
 							// return jwt
 							return jwt.encode({
@@ -35,22 +36,21 @@ module.exports = {
 						});
 				})
 				.catch(function(error) {
-					response.status(500).json({
+					response.serverError({
 						title: 'Failed to login.',
-						reason: error
-					})
+						reason: error.message
+					});
 				})
 				.done(function(token) {
-					token && response.status(200).json({
+					token && response.ok({
 						token: token,
 						expires: expires
-					})
+					});
 				});
-		} 
+		}
 		else {
-			response.badRequest();
+			response.badRequest({reason: 'No \'user\' root object in request body.'});
 			return;
 		}
 	}
 };
-
